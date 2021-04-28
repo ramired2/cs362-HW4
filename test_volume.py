eb6b1ecc463206c05d379b98ea4d3955f54910a0
Daisy <ramired2@oregonstate.edu>
2021-04-28 14:19:42 -0700

    five test cases. 2 Fail, 3 Correct

---
+++ test_volume.py
@@ -0,0 +1,29 @@
+import unittest
+import volume
+
+class testCaseVolume(unittest.TestCase):
+    def test_vol(self):
+    #pass tests
+        # test post simple num
+        # result = volume.vol(3)
+        # self.assertEqual(result, 27)
+
+        # test post complex num
+        # result = volume.vol(9.65)
+        # self.assertEqual(result, 898.6321250000001)
+
+        #test neg numbers
+        # result = volume.vol(-6)
+        # self.assertEqual(result, -216)
+
+    #fail tests
+        # test post complex num
+        # result = volume.vol(9.65)
+        # self.assertEqual(result, 898.632125000000)
+
+        #type error test
+        # result = volume.vol(s)
+        # self.assertEqual(result, s)
+
+if __name__ == "__main__":
+    unittest.main()
