24f50483ce1da9a25e6995c4fce8193ae3e6df8f
Daisy <ramired2@oregonstate.edu>
2021-04-28 19:48:50 -0700

    test cases implemented

--- test_person.py
+++ test_person.py
@@ -1,9 +1,26 @@
 import unittest
-import volume
+import person
 
 class testCasePerson(unittest.TestCase):
     def test_per(self):
-        pass
+        
+        result = person.getInfo()
+
+    # passing test
+        #capital letters test
+        # self.assertEqual("Sarah", result.first)
+        # self.assertEqual("Jane", result.last)
+
+        # capital and lower case test
+        # self.assertEqual("jeSS", result.first)
+        # self.assertEqual("ranDalF", result.last)
+    
+    # fail test - type errors --> adding numbers
+        bIsDigit = False
+
+        if result.first.isdigit() or result.last.isdigit():
+            bIsDigit = True
+        self.assertNotEqual(bIsDigit, True)
 
 if __name__ == "__main__":
     unittest.main()
