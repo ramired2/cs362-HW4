12fdb0196cd91d20f6a593e7d185d367ac6f3ab2
Daisy <ramired2@oregonstate.edu>
2021-04-29 11:44:35 -0700

    re-named cases and removed complex number case

--- test_volume.py
+++ test_volume.py
@@ -4,22 +4,19 @@ import volume
 class testCaseVolume(unittest.TestCase):
     def test_vol(self):
     #pass tests
-        # test post simple num
-        result = volume.vol(3)
-        self.assertEqual(result, 27)
+        # simple num
+        # result = volume.vol(3)
+        # self.assertEqual(result, 27)
 
-        # test post complex num
-        # result = volume.vol(9.65)
-        # self.assertEqual(result, 898.6321250000001)
-
-        #test neg numbers
+        # neg number
         # result = volume.vol(-6)
         # self.assertEqual(result, -216)
 
     #fail test
         #type error test
-        # result = volume.vol(s)
-        # self.assertEqual(result, s)
+        result = volume.vol(s)
+        
+        self.assertEqual(result, s)
 
 if __name__ == "__main__":
     unittest.main()
