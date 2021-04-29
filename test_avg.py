78133a35508e4e51dfeb7a0537ed55f19205ed0b
Daisy <ramired2@oregonstate.edu>
2021-04-29 11:39:43 -0700

    labeled test cases

--- test_avg.py
+++ test_avg.py
@@ -8,29 +8,14 @@ class testCaseAvg(unittest.TestCase):
         result = avg.avg([5, 6, 10, 60])
         self.assertEqual(result, 20.25)
 
-        # result = avg.avg([99])
-        # self.assertEqual(result, 99)
-
         # negative list
         # result = avg.avg([-60, -19, -7, -6, -19, -70, -79])
         # self.assertEqual(result, -37.142857142857146)
 
-        # negative and positive list
-        # result = avg.avg([99, -60, -1, -19, 5, 7, -5])
-        # self.assertEqual(result, 3.7142857142857144)
-
     #Fail tests
-        #empty list
-        # result = avg.avg([])
-        # self.assertEqual(result, 0)
-
         #letters in the list
         # result = avg.avg([s, q, w, l])
         # self.assertEqual(result, 1)
-
-        # letters and numbers
-        # result = avg.avg([-8, q, 20, w, 70, l, -70, 0, 0])
-        # self.assertEqual(result, 2)
 
 if __name__ == "__main__":
     unittest.main()
